6bd824a3df85e9191869055b2379ce2b56e377a8
ekaphopl04 <ekaphop.l04@gmail.com>
2025-06-20 15:52:05 +0700

    feat: add BlogPostConfiguration model class with empty implementation

---
+++ FullTextSearchDemo/Models/BlogPostConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FullTextSearchDemo.Models
+{
+    public class BlogPostConfiguration
+    {
+        
+    }
+}
--- FullTextSearchDemo/obj/Debug/net8.0/FullTextSearchDemo.AssemblyInfo.cs
+++ FullTextSearchDemo/obj/Debug/net8.0/FullTextSearchDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d2c314e17f88004e7e699a85d36ea7e98935ecb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4306d36e84e5c1e3022fbe32091c26a4f69f5f15")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
