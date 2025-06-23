1d5ca1cab62fd20ae0d735cbcbc92c05e143e17b
ekaphopl04 <ekaphop.l04@gmail.com>
2025-06-23 15:14:52 +0700

    chore: update assembly version and cache inputs for build

--- FullTextSearchDemo/obj/Debug/net8.0/FullTextSearchDemo.AssemblyInfo.cs
+++ FullTextSearchDemo/obj/Debug/net8.0/FullTextSearchDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca55ff19acbb208aa8b256c37b43e2ca1cba3ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c24154556d23957c45c96a8012d78ccf55547ee4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FullTextSearchDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
